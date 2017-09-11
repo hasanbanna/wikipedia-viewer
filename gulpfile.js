b30df45fd4f60f993814a7af7c748ba18c0c1f72
Hasan Banna <banna.hsn@gmail.com>
2017-09-11 13:24:11 +0100

    setup scss and jquery

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-
 gulp.task('scripts', function(){
     return gulp.src('src/js/*.js')
         .pipe(concat('main.js'))
@@ -14,9 +13,9 @@ gulp.task('scripts', function(){
 });
 var sass = require('gulp-ruby-sass');
 gulp.task('sass', function(){
-    return sass('src/scss/style.scss', {style: 'compressed'})
+    return sass('src/scss/*.scss', {style: 'compressed'})
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('build/css'));
+        .pipe(gulp.dest('build/scss'));
 });
 
 gulp.task('watch', function(){
