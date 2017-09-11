3eef56b460c3df58889899a4f8755540a6f80799
Hasan Banna <banna.hsn@gmail.com>
2017-09-11 13:48:38 +0100

    added take user to random wikipedia article feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
+	$(".random-article").on('click', function(){
+		window.open('https://en.wikipedia.org/wiki/Special:Random', '_blank');		
+	});
 
+
+// get wikipedia api details
+// $.ajax(
+// 	{ url: "",
+// 	  success: function(res){
+
+// 	  },
+// 	  error:function(res){
+
+// 	  }
+
+// });
 });
